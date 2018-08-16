6595f5dd504125a9127f2470c9ac760d6ec235cd
Uriah Ahrak <uriah.achrak@gmail.com>
2018-08-16 21:08:26 +0300

    Added config for build and serve

---
+++ config/build.js
@@ -0,0 +1,39 @@
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+
+const path = require('path');
+
+// Clean configurations
+const clean_paths = [
+    'dist'
+];
+
+const clean_options = {
+    watch: true
+};
+
+module.exports = {
+    entry: './index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js']
+    },
+    output: {
+        filename: 'dist/index.js',
+        path: path.resolve(__dirname, '..'),
+    },
+    target: 'node',
+    mode: 'production',
+    plugins: [
+        new CleanWebpackPlugin(clean_paths, clean_options),
+        new UglifyJsPlugin(),
+    ]
+};
